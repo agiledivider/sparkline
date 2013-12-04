0f7797b0b6126ce28906541b1e2414b34a954748
Falk Kühnel <fk@cogitatio.de>
2013-12-04 22:15:55 +0100

    added components test

--- test/Svg/SvgTest.php
+++ test/Svg/SvgTest.php
@@ -23,6 +23,18 @@ class SvgTest extends \PHPUnit_Framework_TestCase
             $svg->render()
         );
     }
+
+    public function testSvgComponents()
+    {
+        $svg = new Svg(100, 100);
+        $svg->add(new Path(array(array(0,0),array(100,0))));
+        $svg->add(new Path(array(array(0,100),array(100,100)), array('class' => 'testing')));
+
+        $this->assertEquals(
+            '<?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"><path class="path" d="M0,0L100,0"></path><path class="testing" d="M0,100L100,100"></path></svg>', 
+            $svg->render()
+        );
+    }
   
 }
 
