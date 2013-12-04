b8e3b62c1a0ffe99c578be4cdbe5fcc71359c932
Falk Kühnel <fk@cogitatio.de>
2013-12-04 21:53:25 +0100

    added more tests to path

--- test/Svg/PathTest.php
+++ test/Svg/PathTest.php
@@ -6,6 +6,7 @@ use Cogitatio\Sparkline\Svg\Path;
  */
 class SvgPathTest extends \PHPUnit_Framework_TestCase
 {
+    private $data = array(array(0,0), array(1,2), array(3.12344,1.12344), array(100,40));
     /**
      * @dataProvider pathProvider
      */
@@ -25,6 +26,24 @@ class SvgPathTest extends \PHPUnit_Framework_TestCase
                 array(array(0,0), array(1,2), array(3.1234567,1.1234567), array(100,40)),
                 '<path class="path" d="M0,0L1,2,3.123,1.123,100,40"></path>'
             )
+        );
+    }
+
+    public function testSimplePathWithPrecisionOption()
+    {
+        $path = new Path($this->data, array('precision' => 4));
+        $this->assertEquals(
+            '<path class="path" d="M0,0L1,2,3.1234,1.1234,100,40"></path>', 
+            $path->render()
+        );
+    }
+
+    public function testSimplePathWithClassOption()
+    {
+        $path = new Path($this->data, array('class' => 'something'));
+        $this->assertEquals(
+            '<path class="something" d="M0,0L1,2,3.123,1.123,100,40"></path>', 
+            $path->render()
         );
     }
 }
